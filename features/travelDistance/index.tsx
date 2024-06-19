1eb6d31fcdebcee3894840ff0766964e9b4ad4c4
Marcel Fleuren <marcelfleuren@MacBook-Pro-van-Marcel.local>
2024-06-19 16:29:02 +0200

    add range slider component

--- features/travelDistance/index.tsx
+++ features/travelDistance/index.tsx
@@ -1,5 +1,53 @@
-import { Text } from '@typography/Text'
+import * as React from 'react'
+import { StyleSheet, View } from 'react-native'
+import Slider from '@react-native-community/slider'
+
+import { Title } from '@typography/Title'
+import { Colors } from '@/constants/Colors'
+import { Button } from '@interaction/Button'
+import { useOnboarding } from '@/providers/onboardingProvider'
 
 export const TravelDistance = ({ onNextStep }: { onNextStep: () => void }) => {
-  return <Text>Hoever wil je reizen?</Text>
+  const { range, onboardingAction } = useOnboarding()
+  const [value, setValue] = React.useState(range)
+
+  const handleNextStep = () => {
+    onboardingAction({
+      type: 'SET_RANGE',
+      payload: value,
+    })
+    onNextStep()
+  }
+
+  return (
+    <>
+      <View style={styles.formWrapper}>
+        <Title>{value} km</Title>
+        <Slider
+          style={{ width: '100%', height: 60 }}
+          minimumValue={0}
+          step={1}
+          maximumValue={100}
+          minimumTrackTintColor={Colors.brand}
+          maximumTrackTintColor={Colors.lightGray}
+          onValueChange={setValue}
+          value={value}
+        />
+      </View>
+      <Button
+        disabled={value === range}
+        title="Volgende stap"
+        onPress={handleNextStep}
+      />
+    </>
+  )
 }
+
+const styles = StyleSheet.create({
+  inputField: {
+    height: 'auto',
+  },
+  formWrapper: {
+    flexGrow: 1,
+  },
+})
